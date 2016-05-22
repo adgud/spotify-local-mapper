3f1d32fe79f0a1f7cdc9634f4be3505e48a41efb
adgud <a.guderski@gmail.com>
2016-05-22 02:58:39 +0200

    redone spotify auth

--- spotify.py
+++ spotify.py
@@ -1,10 +1,8 @@
 import requests
 import sys
 import json
-import random
 import urllib.parse
 import webbrowser
-import time
 
 
 class Spotify:
@@ -12,9 +10,14 @@ class Spotify:
     __API_URL = 'https://api.spotify.com/v1/'
     __AUTH_URL = 'https://accounts.spotify.com/authorize'
     __access_token = None
+    __client_id = '6224d6d8e66045b59ce328d043aa4b5d'
+    __redirect_url = 'https://adgud.github.io/spotify-local-mapper/callback/'
+    __username = None
 
-    def __init__(self):
-        pass
+    def __init__(self, client_id=None, redirect_url=None):
+        if client_id is not None and redirect_url is not None:
+            self.__client_id = client_id
+            self.__redirect_url = redirect_url
 
     def search(self, query_string, search_limit=1):
         params = {
@@ -31,7 +34,7 @@ class Spotify:
         response_json = search_request.json()
 
         if response_json['tracks']['total'] == 0:
-            print('nothing found:',query_string, file=sys.stderr)
+            print('nothing found:', query_string, file=sys.stderr)
             return None
 
         search_results = []
@@ -57,45 +60,30 @@ class Spotify:
         else:
             return search_results
 
-    def auth(self, client_id, redirect_uri, dialog=False):
-        # use implicit grant
-        response_type = 'token'
-        scope = 'playlist-read-private playlist-modify-private'
-        state = str(random.getrandbits(256))
-        show_dialog = 'true' if dialog else 'false'
+    def auth(self, username, dialog=False):
+        # uses implicit grant
+        self.__username = username
         params = {
-            'client_id': client_id,
-            'response_type': response_type,
-            'redirect_uri': redirect_uri,
-            'state': state,
-            'scope': scope,
-            'show_dialog': show_dialog
+            'client_id': self.__client_id,
+            'response_type': 'token',
+            'redirect_uri': self.__redirect_url,
+            'scope': 'playlist-read-private playlist-modify-private',
+            'show_dialog': 'true' if dialog else 'false'
         }
         auth_url = self.__AUTH_URL + '?' + urllib.parse.urlencode(params)
-        print('waiting for authorization...')
         webbrowser.open(auth_url)
-        auth_check_url = redirect_uri + '?state=' + state
-        # start polling
-        i = 0
-        while i < 10:
-            time.sleep(3)
-            auth_check_request = requests.get(auth_check_url)
-
-            if auth_check_request.status_code == 200:
-                self.__access_token = auth_check_request.text
-                return
-
-            i += 1
-
-        print('timeout when waiting for access token')
+        self.__access_token = input('paste token here: ')
 
     def get_access_token(self):
         return self.__access_token
 
-    def create_playlist(self, username, playlist_name, public=False):
+    def get_username(self):
+        return self.__username
+
+    def create_playlist(self, playlist_name, public=False):
         if self.__access_token is None:
             print('not authorized yet', file=sys.stderr)
-            raise ValueError
+            return None
 
         headers = {
             'Authorization': 'Bearer ' + self.__access_token,
@@ -106,19 +94,20 @@ class Spotify:
             'public': 'true' if public else 'false'
         }
         payload = json.dumps(data)
-        url = self.__API_URL + 'users/' + username + '/playlists'
+        url = self.__API_URL + 'users/' + self.__username + '/playlists'
         create_playlist_request = requests.post(url, data=payload, headers=headers)
         response = create_playlist_request.json()
         if create_playlist_request.status_code == 201:
+            print('playlist "' + playlist_name + '" created')
             return response['id']
         else:
             print(response, file=sys.stderr)
             return None
 
-    def add_tracks_to_playlist(self, username, playlist_id, track_ids, position=None):
+    def add_tracks_to_playlist(self, playlist_id, track_ids, position=None):
         if self.__access_token is None:
             print('not authorized yet', file=sys.stderr)
-            raise ValueError
+            return None
 
         headers = {
             'Authorization': 'Bearer ' + self.__access_token,
@@ -138,7 +127,7 @@ class Spotify:
             'uris': uris
         }
 
-        url = self.__API_URL + 'users/' + username + '/playlists/' + playlist_id + '/tracks'
+        url = self.__API_URL + 'users/' + self.__username + '/playlists/' + playlist_id + '/tracks'
         add_tracks_request = requests.post(url, params=params, headers=headers)
         response = add_tracks_request.json()
         if add_tracks_request.status_code == 201:
