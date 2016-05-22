fbdb5df61b8f6476f0ab5a93cf28ae0e8db2a7ce
Adam Guderski <a.guderski@gmail.com>
2016-05-22 16:54:14 +0200

    some refactoring, input validation, is working now

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # spotify-local-mapper
-Map you local media files into a playlist on Spotify using Python 3
 
-***
+## Description
+A simple tool to map you local media files into a playlist on Spotify using Python 3.
 
-This is work in progress project and does not do much yet.
+## Installation
+Make sure that the dependecies are installed, then simply download the script and run it. 
 
-### Depends on:
-* requests
-* mutagen
-* spotipy
+### Dependencies
+* [mutagen](https://github.com/nex3/mutagen)
+* [spotipy](https://github.com/plamere/spotipy)
+
+## Todo
+* Convert to proper package, so that dependencies can be installed automatically
+* Include interactive mode, where user can select which track should be mapped and change search query
+* Cache the Spotify authorization token
--- spotify_local_mapper.py
+++ spotify_local_mapper.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 import sys
 import urllib
 import webbrowser
@@ -28,28 +29,26 @@ def read_id3(filename):
     try:
         audio = ID3(filename)
     except (ID3NoHeaderError, FileNotFoundError) as e:
-        print('failed to read ID3 data from', filename, ':', str(e), file=sys.stderr)
+        print('Failed to read ID3 data from', filename, ':', str(e), file=sys.stderr)
         return None
 
     title, artist, album = '', '', ''
-    # todo: properly convert to utf8
     try:
         title = audio["TIT2"].text[0]
     except KeyError as e:
-        print('failed to read ID3 tag', str(e), 'from:', filename, file=sys.stderr)
+        print('Failed to read ID3 tag', str(e), 'from:', filename, file=sys.stderr)
 
     try:
         artist = audio['TPE1'].text[0]
     except KeyError as e:
-        print('failed to read ID3 tag', str(e), 'from:', filename, file=sys.stderr)
+        print('Failed to read ID3 tag', str(e), 'from:', filename, file=sys.stderr)
 
     try:
         album = audio['TALB'].text[0]
     except KeyError as e:
-        print('failed to read ID3 tag', str(e), 'from:', filename, file=sys.stderr)
+        print('Failed to read ID3 tag', str(e), 'from:', filename, file=sys.stderr)
 
     if title == '' and artist == '' and album == '':
-        print('no tags found for file', filename, file=sys.stderr)
         return None
 
     return {'artist':artist, 'title':title, 'album': album}
@@ -71,33 +70,54 @@ def get_spotify_auth_token(client_id, redirect_url, dialog=False):
 def main():
     music_dir = input('Directory with MP3 files: ')
     mp3_files = get_mp3_files(music_dir)
+    if len(mp3_files) == 0:
+        print('No MP3 files found in', music_dir, file=sys.stderr)
+        return
 
     playlist_name = input('Playlist name to be created: ')
-    spotify_username = input('Spotify username: ')
+    if playlist_name is '':
+        playlist_name = 'imported tracks'
 
+    # you can use your own application created at https://developer.spotify.com/my-applications/
     client_id = '6224d6d8e66045b59ce328d043aa4b5d'
     redirect_url = 'https://adgud.github.io/spotify-local-mapper/callback/'
+
+    # use own implementation of getting auth token, because the spotipy's is buggy
     token = get_spotify_auth_token(client_id, redirect_url)
     sp = spotipy.Spotify(auth=token)
-    playlist = sp.user_playlist_create(spotify_username, 'test', public=False)
+
+    try:
+        user_profile = sp.me()
+    except spotipy.client.SpotifyException:
+        print('Not authorized - did you paste a valid token?', file=sys.stderr)
+        return
+
+    spotify_user_id = user_profile['id']
+    playlist = sp.user_playlist_create(spotify_user_id, playlist_name, public=False)
     playlist_id = playlist['id']
 
+    files_added = 0
     for file in mp3_files:
-        print('\nProcessing ', file, '...')
+        print('\nProcessing', file, '...')
         tags = read_id3(file)
         if tags is None:
+            print('No tags found for file', file, file=sys.stderr)
             continue
 
         query = tags['artist'] + ' ' + tags['title']
         search_result = sp.search(query)
         if len(search_result['tracks']['items']) == 0:
-            print('Nothing found for "', query, '"', file=sys.stderr)
+            print('Nothing found for "', query, '"', sep='', file=sys.stderr)
             continue
 
         track_uri = search_result['tracks']['items'][0]['uri']
-        sp.user_playlist_add_tracks(spotify_username, playlist_id, [track_uri])
-        print('Added "', search_result['tracks']['items'][0]['name'], '" by ', search_result['tracks']['items'][0]['artists'][0]['name'])
+        sp.user_playlist_add_tracks(spotify_user_id, playlist_id, [track_uri])
+        artist = search_result['tracks']['items'][0]['artists'][0]['name']
+        title = search_result['tracks']['items'][0]['name']
+        print('Added "', title, '" by ', artist, sep='')
+        files_added += 1
 
+    print('\nFinished, successfully added', files_added, 'out of', len(mp3_files), 'files.')
 
 if __name__ == '__main__':
     main()
